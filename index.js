4b4aaa1ba2fb07b10567a6542baffb15bb9710c8
anton <antonr@trafficpoint.io>
2020-08-10 19:39:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ const puppeteer = require('puppeteer');
   })
   const page = await browser.newPage()
   await page.setViewport({ width: 1280, height: 1800 })
-  // await page.goto('https://getbootstrap.com/docs/4.3/components/forms/#checkboxes-and-radios')
-  await page.goto('https://brooklynbedding.com/products/brooklyn-signature-hybrid?sscid=81k4_3dh82&utm_campaign=314743&utm_medium=referral&utm_source=shareasale.com&variant=32591736569901')
-  // const element = await page.$("#price > div > span");
-  const innerText = await page.evaluate(() => document.querySelector('#price > div > span').innerText);
-
-  // const text = await page.evaluate(element => element.textContent, element);
+  await page.goto('https://getbootstrap.com/docs/4.3/components/forms/#checkboxes-and-radios')
+  const innerText = await page.evaluate(() => document.querySelector('#checkboxes-and-radios > span').innerText);
   console.log(innerText)
   const title = await page.title()
   console.log(title)
-  // await page.screenshot({
-  //   fullPage: false,
-  //   path:`screenshot.png`
-  // });
   await browser.close()
 })()
